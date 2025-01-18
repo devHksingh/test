7ca92027735c794b1f7271cef35df383a98a95fe
devHksingh <hksingh.dev@gmail.com>
2025-01-18 23:48:13 +0530

    modify display

--- src/components/FormDetails.jsx
+++ src/components/FormDetails.jsx
@@ -10,14 +10,18 @@ const FormDetails = () => {
   return (
     <div className="max-w-4xl mx-auto space-y-4">
       <h2 className="p-2 text-2xl font-semibold text-center">FormDetails</h2>
-      <div className="flex items-center justify-center gap-4">
+      <div className="grid items-center justify-center gap-4 grid-col-1 md:grid-cols-3">
       {formState.map((item,index)=>(
         <div className="flex flex-col gap-4 p-4 border rounded-md" key={index}>
             <h4>User Name: {item.name}</h4>
-            <h4>User roll: {item.roll}</h4>
+            <h4>User age: {item.age}</h4>
+            <h4>User email: {item.email}</h4>
             <div className="space-x-4">
-              <button className="p-1 px-2 bg-red-500 rounded-md hover:bg-red-600" onClick={()=>handleRemoveData(item.name,item.roll)}>Delete</button>
-              <Link className="p-1 px-2 rounded-md bg-sky-500 hover:bg-sky-600" to={`/${item.roll}`}>View</Link>
+              <button className="p-1 px-2 bg-red-500 rounded-md hover:bg-red-600" onClick={()=>{
+                console.log(index)
+                handleRemoveData(index)
+                }}>Delete</button>
+              <Link className="p-1 px-2 rounded-md bg-sky-500 hover:bg-sky-600" to={`/${index}`}>View</Link>
             </div>
         </div>
       ))}
--- src/components/SingleUserDetails.jsx
+++ src/components/SingleUserDetails.jsx
@@ -1,25 +1,39 @@
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 import { useForm } from "../context/formContext";
 import { useState } from "react";
 
 const SingleUserDetails = () => {
-    const {userRoll} = useParams()
-    const {formState,handleAddData} = useForm()
-    const [userName,setUserName] = useState("")
-    const navigate = useNavigate()
-    console.log(userRoll);
+  const { index } = useParams();
+  const { formState, handleUpdateData } = useForm();
+  const [userName, setUserName] = useState(`${formState.at(index).name}`);
+  const [userAge, setUserAge] = useState(`${formState.at(index).age}`);
+  const [userEmail, setUserEmail] = useState(`${formState.at(index).email}`);
+  const navigate = useNavigate();
+  console.log(index);
 
-    const userData = formState.filter((data)=> data.roll === userRoll)
-    console.log(formState);
-    console.log(userData[0].roll);
-    const handleUpdate =()=>{
-        handleAddData(userName,userData[0].roll)
-        navigate('/show')
-    }
+  // const userData = formState.filter((data)=> data.roll === userRoll)
+  const userData = formState.at(index);
+  console.log("Single User Details", formState);
+  console.log("Single User Details", userData);
+  const handleUpdate = () => {
+    // handleAddData(userName,userData[0].roll)
+    // navigate('/show')
+    console.log(userName);
+    console.log(userData.name);
+    const name = userName || userData.name;
+    const age = userAge || userData.age;
+    const email = userEmail || userData.email;
+    console.log("Single User Details update function",name, age, email);
+    handleUpdateData(index, name, age, email);
+    navigate("/show");
+  };
+  const handleCancel = () => {
+    navigate("/show");
+  };
   return (
     <div className="max-w-4xl p-4 mx-auto space-y-4 text-center">
-        <h2 className="text-2xl font-medium text-center">User Details</h2>
-            {userData.map((item,index)=>(
+      <h2 className="text-2xl font-medium text-center">User Details</h2>
+      {/* {userData.map((item,index)=>(
                 <div  key={index} className="flex flex-col items-center justify-center max-w-md gap-4 p-4 mx-auto border rounded-md">
                     <label className="flex flex-col justify-start w-full">
                         <span className="block text-left opacity-80">UserName:</span> 
@@ -36,10 +50,65 @@ const SingleUserDetails = () => {
                     
                     <button className="p-1 px-2 bg-blue-500 rounded-md hover:bg-blue-600" onClick={handleUpdate}>Update</button>
                 </div>
-            ))}
-            <p className="capitalize "><span className="font-medium text-orange-400 underline underline-offset-4">CAUTION</span> : you can update username only</p>
-    </div>
-  )
-}
+            ))} */}
+      <div
+        
+        className="flex flex-col items-center justify-center max-w-md gap-4 p-4 mx-auto border rounded-md"
+      >
+        <label className="flex flex-col justify-start w-full">
+          <span className="block text-left opacity-80">UserName:</span>
+          <input
+            type="text"
+            placeholder={userData.name}
+            value={userName}
+            className="p-1 text-black rounded-md cursor-pointer bg-stone-400 placeholder:text-black"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </label>
+        <label className="flex flex-col justify-start w-full">
+          <span className="block text-left opacity-80">UserAge:</span>
+          <input
+            type="text"
+            value={userAge}
+            onChange={(e) => setUserAge(e.target.value)}
+            className="p-1 text-black rounded-md cursor-pointer bg-stone-400"
+          />
+        </label>
+        <label className="flex flex-col justify-start w-full">
+          <span className="block text-left opacity-80">UserEmail:</span>
+          <input
+            type="text"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+            className="p-1 text-black rounded-md cursor-pointer bg-stone-400"
+          />
+        </label>
 
-export default SingleUserDetails
+        <div className="flex gap-4">
+          <button
+            className="p-1 px-2 bg-blue-500 rounded-md hover:bg-blue-600"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+          <button
+            className="p-1 px-2 rounded-md bg-rose-500 hover:bg-rose-600"
+            onClick={handleUpdate}
+          >
+            Update
+          </button>
+        </div>
+      </div>
+
+      <p className="capitalize ">
+        <span className="font-medium text-orange-400 underline underline-offset-4">
+          CAUTION
+        </span>{" "}
+        : Once updated you can not get back old value
+      </p>
+    </div>
+    // <></>
+  );
+};
+
+export default SingleUserDetails;
